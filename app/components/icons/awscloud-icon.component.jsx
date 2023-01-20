c75a1278f514be367e12afb23e9e91e721c95ede
Farzpal <113582889+Mehakpal123@users.noreply.github.com>
2023-01-20 18:13:33 +0530

    Delete app/components

--- app/components/icons/awscloud-icon.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import AWSCloud from 'react-aws-icons/dist/aws/compute/AWSCloud';
-import { Handle } from 'reactflow';
-
-const AWSCloudIcon = () => <AWSCloud size={70} style={{ position: 'absolute' }} />
-
-
-export default AWSCloudIcon 
