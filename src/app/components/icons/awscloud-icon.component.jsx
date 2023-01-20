c439594e28ac9ac665a9ec56909c37688b3e8ffe
Farzpal <113582889+Mehakpal123@users.noreply.github.com>
2023-01-20 18:28:14 +0530

    Added customization to aws cloud icon

--- src/app/components/icons/awscloud-icon.component.jsx
+++ src/app/components/icons/awscloud-icon.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AWSCloud from 'react-aws-icons/dist/aws/compute/AWSCloud';
 import { Handle } from 'reactflow';
 
-const AWSCloudIcon = () => <AWSCloud size={70} style={{ position: 'absolute' }} />
+const AWSCloudIcon = () => <AWSCloud size={80} style={{ position: 'absolute', marginLeft: '-31px', marginTop: '-16px' }} />
 
 
 export default AWSCloudIcon 
