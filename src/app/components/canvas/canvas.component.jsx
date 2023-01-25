b452450f0dbc878a90a20770c1634edf81dc5f51
Farzpal <113582889+cdeveloper123@users.noreply.github.com>
2023-01-25 10:38:24 +0530

    added elk js to flow

--- src/app/components/canvas/canvas.component.jsx
+++ src/app/components/canvas/canvas.component.jsx
@@ -3,17 +3,19 @@ import ReactFlow, { Background, Controls, MarkerType, applyEdgeChanges, applyNod
 import 'reactflow/dist/style.css';
 import './canvas.component.css';
 import nodeTypes from "../icons/icons";
-// import nodeTypes from "../../../common/flowData.js";
+import ELK from 'elkjs';
 
 function Canvas() {
 
 	const [nodes, setNodes] = useState({});
 	const [edges, setEdges] = useState({});
+
+
 	useEffect(() => {
 		fetch(`http://localhost:5000/get-flow-data`)
 			.then((response) => response.json())
 			.then((data) => {
-				// data.nodes.map(node => node.id !==1 && node.position = { x: 0, y: 0 });
+				// data.nodes.map(node => node.position = { x: 0, y: 0 });
 				data.edges.map(edge => {
 					edge.label = edge.data.label;
 					edge.markerEnd = {
@@ -22,17 +24,42 @@ function Canvas() {
 					}
 					return;
 				});
-				setNodes(data.nodes);
-				setEdges(data.edges);
+
+				const elk = new ELK({
+					defaultLayoutOptions: {
+						"elk.algorithm": "layered",
+						"elk.direction": "BOTTOM",
+						"elk.layered.spacing.nodeNodeBetweenLayers": 170,
+						"elk.spacing.nodeNode": "100",
+
+					}
+				});
+				const graph = {
+					id: "root",
+					children: data.nodes,
+					edges: data.edges
+				}
+
+				elk.layout(graph).then((graph) => {
+					graph.children.map((node) => node.position = { x: node.x, y: node.y })
+					graph.edges.map((edge) => edge)
+					setNodes(graph.children);
+					setEdges(graph.edges);
+				}).catch(console.error)
 			})
 	}, [])
+
+	useEffect(() => {
+		console.log('nodes---', nodes);
+		console.log('edges---', edges);
+	}, [nodes, edges])
 
 	const onNodesChange = useCallback((changes) => setNodes((nds) => applyNodeChanges(changes, nds)), []);
 	const onEdgesChange = useCallback((changes) => setEdges((eds) => applyEdgeChanges(changes, eds)), []);
 
 	return (
 		<div className='canvas__main-div'>
-			{nodes.length > 0 && edges.length > 0 ? <ReactFlow nodeTypes={nodeTypes} nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} fitView>
+			{(nodes && nodes.length > 0) && (edges && edges.length) > 0 ? <ReactFlow nodeTypes={nodeTypes} nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} fitView>
 				<Background />
 				<Controls />
 			</ReactFlow> : <p>Loading</p>}
